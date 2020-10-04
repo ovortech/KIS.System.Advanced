a45b39c976e568a0d67ef9549185f3a19110a4b9
rubens <rubensf@outlook.com>
2020-10-04 20:44:50 -0300

    mapeamento rubens

---
+++ KIS.System.Advanced.Domain/Interfaces/IFuncaoRepository.cs
@@ -0,0 +1,14 @@
+﻿using KIS.System.Advanced.Domain.Entities;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace KIS.System.Advanced.Domain.Interfaces
+{  
+    
+    public interface IFuncaoRepository : IRepositoryBase<Funcao>
+    {
+
+      //  IEnumerable<Funcao> BuscarPorNome(string DESC_FUNCAO);  OBS: verificar função
+    }
+    
+}
---
+++ KIS.System.Advanced.Infra.Data/EntityConfig/ContratoMapping.cs
@@ -0,0 +1,22 @@
+﻿using KIS.System.Advanced.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KIS.System.Advanced.Infra.Data.EntityConfig
+{
+    public class ContratoMapping : EntityTypeConfiguration<Contrato>
+    {
+        public ContratoMapping()
+        {
+            ToTable("CONTRATO");
+            HasKey(e => e.ID_CONTRATO);
+            Property(e => e.ID_PEDIDO_CONTRATO).IsRequired();
+            Property(e => e.ID_CLIENTE_CONTRATO).IsRequired();
+            Property(e => e.FATURADO_CONTRATO).IsRequired();
+        }
+    }
+}
