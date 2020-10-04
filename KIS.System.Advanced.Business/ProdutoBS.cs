c4a266b8b4496b56442d1b3c7bc229f1be41604f
Everton Brzozowy Alves <everton.brzozowy.alves@gmail.com>
2020-10-04 18:16:11 -0300

    Configurações básicas para a entidade Produto.

---
+++ KIS.System.Advanced.Business/ProdutoBS.cs
@@ -0,0 +1,16 @@
+﻿using KIS.System.Advanced.Domain.Interfaces;
+using KIS.System.Advanced.Infra.Data.Repositories;
+using System;
+
+namespace KIS.System.Advanced.Business
+{
+    public class ProdutoBS
+    {
+        IProdutoRepository dbProduto;
+        public ProdutoBS()
+        {
+            dbProduto = new ProdutoRepository();
+        }
+
+    }
+}
---
+++ KIS.System.Advanced.Infra.Data/Repositories/ProdutoRepository.cs
@@ -0,0 +1,19 @@
+﻿using KIS.System.Advanced.Domain.Entities;
+using KIS.System.Advanced.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KIS.System.Advanced.Infra.Data.Repositories
+{
+    public class ProdutoRepository : RepositoryBase<Produto>, IProdutoRepository
+    {
+        public ProdutoRepository() : base()
+        {
+
+        }
+    
+    }
+}
