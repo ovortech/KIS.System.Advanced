047b1eecc67e8c928ec12524ccea41f602ecc075
gui_c <ovortech@gmail.com>
2020-10-11 02:39:52 -0300

    Verificação de bd e inclusão de item pedido e comissão na parte de testes.

---
+++ KIS.System.Advanced.Domain/Interfaces/IItemPedidoRepository.cs
@@ -0,0 +1,14 @@
+﻿using KIS.System.Advanced.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KIS.System.Advanced.Domain.Interfaces
+{
+    public interface IItemPedidoRepository : IRepositoryBase<ItemPedido>
+    {
+
+    }
+}
---
+++ Kist.System.Adavanced.Test/Data/ItemPedidoTest.cs
@@ -0,0 +1,51 @@
+﻿using KIS.System.Advanced.Domain.Entities;
+using KIS.System.Advanced.Infra.Data.Repositories;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kist.System.Adavanced.Test.Data
+{
+    [TestClass]
+    public class ItemPedidoTest
+    {
+        protected ItemPedidoRepository _ItemPedidoRepository;
+        [TestInitialize]
+        public void Initialize()
+        {
+            _ItemPedidoRepository = new ItemPedidoRepository();
+        }
+
+        [TestMethod]
+        public void AddUser()
+        {
+           
+            var itemPedidos = _ItemPedidoRepository.GetAll();
+            ItemPedido itemPedido = new ItemPedido
+            {
+                ID_PEDIDO = 1,
+                ID_PRODUTO = 1,
+                OBS_PRODUTO = "nada",
+                QTD_PEDIDO = 1,
+                VALOR_UN_PEDIDO = 10,
+                DESCONTO_PEDIDO = 1,
+            };
+
+        _ItemPedidoRepository.Add(itemPedido);
+            var itemPedidosdepois = _ItemPedidoRepository.GetAll();
+
+            Assert.IsTrue(itemPedidos.Count() < itemPedidosdepois.Count());
+        }
+
+
+        [TestMethod]
+        public void GetAllUser()
+        {
+            var aa = _ItemPedidoRepository.GetAll().ToList();
+            Assert.IsTrue(aa != null);
+        }
+    }
+}
