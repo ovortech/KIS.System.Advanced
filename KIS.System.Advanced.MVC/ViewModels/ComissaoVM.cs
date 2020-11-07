593fe1b46de71be50413d3ee0081929fe98b8d1d
Marcio Nogueira de Melo <marciondemelo@gmail.com>
2020-11-07 11:39:30 -0300

    atrabalhando na tela de comissão... em andamento

--- KIS.System.Advanced.MVC/ViewModels/ComissaoVM.cs
+++ KIS.System.Advanced.MVC/ViewModels/ComissaoVM.cs
@@ -7,5 +7,16 @@ namespace KIS.System.Advanced.MVC.ViewModels
 {
     public class ComissaoVM
     {
+        public int Id { get; set; }
+        public DateTime DataVenda { get; internal set; }
+        public string TipoVenda { get; set; }
+        public string NomeProduto { get; set; }
+        public string Descricao { get; set; }
+        public int Quantidade { get; set; }
+        public double ValorVenda { get; set; }
+        public double ValorCustoUnitario { get; set; }
+        public double ValorLucro { get; set; }
+        public double PercComissao { get; set; }
+        public bool Pago { get; set; }
     }
 }
