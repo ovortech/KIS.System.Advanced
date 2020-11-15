9f87b9089f562e6a66680ad6bab7e441df1e9fde
Marcio Nogueira de Melo <marciondemelo@gmail.com>
2020-11-15 14:12:20 -0300

    ajuste de data em globalizze

--- KIS.System.Advanced.MVC/Support/CustomExtensions.cs
+++ KIS.System.Advanced.MVC/Support/CustomExtensions.cs
@@ -8,7 +8,7 @@ namespace KIS.System.Advanced.MVC.Support
     public static class CustomExtensions
     {
         public static List<string> ToListFlag(this Enum enumFlags)
-        {         
+        {
             List<string> result = new List<string>();
             foreach (var item in Enum.GetValues(enumFlags.GetType()))
             {
@@ -16,6 +16,22 @@ namespace KIS.System.Advanced.MVC.Support
                 // if ((enumFlags.GetType() enumFlags & item) != 0) result.Add(item.ToString());
             }
             return result;
+        }
+
+        public static DateTime ParseDateTimeBrToUs(this String data)
+        {
+            int dtDia, dtMes, dtAno;
+            dtDia = Convert.ToInt32(data.Split('/')[0]);
+            dtMes = Convert.ToInt32(data.Split('/')[1]);
+            dtAno = Convert.ToInt32(data.Split('/')[2]);
+            DateTime dt = new DateTime(dtAno, dtMes, dtDia);
+            return dt;
+        }
+
+        public static String ParseDateTimeToBr(this DateTime data)
+        {
+            String dt = string.Format("{0}/{1}/{2}", data.Day, data.Month, data.Year);
+            return dt;
         }
     }
 }
