a1eda29bf10c163d6109924402b09d8f61659de3
Everton Brzozowy Alves <everton.brzozowy.alves@gmail.com>
2020-11-24 22:05:48 -0300

    Criada a parte backend de despesas.

---
+++ KIS.System.Advanced.MVC/ViewModels/DespesaVM.dependence.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KIS.System.Advanced.MVC.ViewModels
+{
+    public class TipoDespesaVM
+    {
+        public int Id { get; set; }
+        public string NomeDespesa{ get; set; }
+    }
+}
